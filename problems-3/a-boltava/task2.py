40e76e96687abacb4e1e0a50f20c6f91bb8f7215
Alexey Boltava <goodmoveab@gmail.com>
2018-04-13 16:39:26 +0700

    Simplify decile counting algorithm

--- problems-3/a-boltava/task2.py
+++ problems-3/a-boltava/task2.py
@@ -35,10 +35,6 @@ def count_decile(file: io.IOBase) -> int:
     # heap stores 10% of max elements of the entire data set
     heap_size = int(math.ceil(dataset_size * 0.1))
     heap = [0] * heap_size
-    # a map to keep track of item repetitions
-    heap_counters = {0: heap_size}
-    # repetitions of current heap-min element in the entire data set
-    heap_min_count = heap_size
 
     while True:
         line = str(file.readline())
@@ -49,38 +45,6 @@ def count_decile(file: io.IOBase) -> int:
             continue
 
         current_value = int(line.split(' ')[2])
-        print(current_value)
+        heapq.heappushpop(heap, current_value)
 
-        heap_min = heap[0]
-        if current_value > heap_min:
-            last_min = heapq.heappushpop(heap, current_value)
-            heap_counters[current_value] = heap_counters.get(current_value, 0) + 1
-            heap_counters[last_min] = heap_counters[last_min] - 1
-            if heap_counters[last_min] == 0:
-                del heap_counters[last_min]
-                heap_min_count = heap_counters[heap[0]]
-        elif current_value == heap_min:
-            heap_min_count += 1
-
-    decile = None
-    if heap_min_count > heap_size:
-        # min element of 10% max elements appeared more than 10% times
-        decile = heap[0] + 1
-    elif heap_min_count == heap_size:
-        # min element of 10% max elements appeared exactly 10% times
-        decile = heap[0]
-    else:
-        # min element of 10% max elements appears in the heap and
-        # might be below the heap as well
-        diff = heap_min_count - heap_counters[heap[0]]
-        if sum(heap_counters.values()) + diff > heap_size:
-            current_min = heap[0]
-            next_min = heapq.heappop(heap)
-            while current_min == next_min:
-                    next_min = heapq.heappop(heap)
-            decile = next_min
-        else:
-            decile = heap[0]
-
-    return decile
-
+    return heap[0]
