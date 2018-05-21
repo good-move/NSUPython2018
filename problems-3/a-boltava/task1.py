14e81643fa46ce5df7877095d38b3e1331e23172
Alexey Boltava <goodmoveab@gmail.com>
2018-05-21 14:00:08 +0700

    Add main function to make code testing easier

--- problems-3/a-boltava/task1.py
+++ problems-3/a-boltava/task1.py
@@ -1,7 +1,10 @@
+import sys
+
+
 def get_statistics(file):
     # skip all lines including first "open ..."
     while True:
-        line = str(file.readline())
+        line = file.readline()
         if line == '' or line.startswith("open"):
             break
 
@@ -32,3 +35,19 @@ def get_statistics(file):
         variance = EX2 - EX ** 2
 
     return mean, variance
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        file_name = sys.argv[1]
+        file = open(file_name, "r")
+        try:
+            mean, variance = get_statistics(file)
+            print("Mean: " + str(mean))
+            print("Variance: " + str(variance))
+        except Exception as e:
+            print(e, file=sys.stderr)
+
+    else:
+        print("Usage: python {0} path".format(sys.argv[0]))
+        print("path - path to file to get statistics from")
